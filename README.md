966b4c5b4a00f6e0be41df804f7833cd6e18d8ef
Sebastien Moran <smoran@sierrawireless.com>
2015-08-25 10:52:34 +0200

    Use bluebird for promises + more methods implemented
    Also added the possibility to directly provide a token to the authenticate function

--- README.md
+++ README.md
@@ -5,6 +5,52 @@ AirVantage.js
 
 Full AirVantage API documentation: https://doc.airvantage.net/av/reference/cloud/API/
 
-More to come... :)
+## Usage
+
+```javascript
+
+var AirVantage = require("airvantage.js");
+
+var config = {
+    serverUrl: "https://eu.airvantage.net", // or https://na.airvantage.net
+    credentials: {
+        client_id: "YOUR_CLIENT_ID",
+        client_secret: "YOUR_CLIENT_SECRET",
+        username: "you@domain.com",
+        password: "yOuRs3cR3t!"
+    }
+};
+
+var airvantage = new AirVantage(config);
+
+airvantage.authenticate()
+    .then(function(){
+        return airvantage.querySystems({
+                labels : ["demo"]
+            })
+    })
+    .then(function(systems){
+        console.log("All demo systems:", systems);
+    });
+
+```
+
+## Available methods
+> See https://doc.airvantage.net/av/reference/cloud/API/ for the methods arguments.
+
+### querySystems
+### queryGateways
+### querySusbscriptions
+### queryApplications
+### createSystems
+### createGateways
+### createSusbscriptions
+### createApplications
+### deleteSystem
+### deleteGateway
+### deleteSusbscription
+### deleteApplication
+### editApplicationCommunication
+### editApplicationData
 
 
--- test/airvantageTests.js
+++ test/airvantageTests.js
@@ -2,55 +2,145 @@ var AirVantage = require("../lib/airvantage");
 var config = require("./config.js");
 var _ = require("lodash");
 
+
+var applicationUid = "";
+// Used to label the resources created for this simulation
+var label = "airvantage.js";
+
 var airvantage = new AirVantage(config);
+var airvantage2 = new AirVantage({
+    serverUrl: config.serverUrl
+});
+var firstToken = "";
 
 airvantage.authenticate()
     .then(function(token) {
-        console.log("token:", token);
-    })
-    .then(getSystems)
-    .then(function(systems) {
-        console.log("systems:", _.pluck(systems, "name"));
+        console.log("Got token:", token);
+        firstToken = token;
     })
     .then(createApplication)
     .then(editCommunication)
-    .then(function() {
-        return getApplications({
-            name: "FakeApp"
-        });
-    })
-    .then(function(application) {
-        console.log("application:", application);
-        return airvantage.deleteApplication(application[0].uid);
-    })
+    .then(editData)
+    .then(createSystem)
+    .then(cleanResources)
+    .then(testBypassAuthentication)
     .catch(function(error) {
-        console.error("# ERROR:", error);
+        console.error("# ERROR:", error.response.body);
     });
+
+/**
+ * Use a second AirVantage instance using first one's token
+ */
+function testBypassAuthentication() {
+    return airvantage2.authenticate(firstToken)
+        .then(function(token) {
+            console.log("Is first token used? ", token === firstToken ? true : false);
+        })
+        .then(function() {
+            return airvantage2.querySystems();
+        })
+        .then(function(systems) {
+            console.log("Found systems with AirVantage client 2:", _.pluck(systems, "name"));
+        });
+}
 
 
 // Helpers
-function getSystems() {
-    return airvantage.querySystems();
-}
 
 function createApplication() {
     return airvantage.createApplication({
-        "name": "FakeApp",
-        "revision": "0.3",
-        "type": "SBZ1"
-    });
+            "name": "FakeApp",
+            "revision": "0.3",
+            "type": "SBZ1",
+            "labels": [label]
+        })
+        .then(function(application) {
+            console.log("Created application:", application);
+            applicationUid = application.uid;
+            return application;
+        });
 }
 
-function getApplications(query) {
-    return airvantage.queryApplications(query);
-}
-
-function editCommunication(application) {
-    return airvantage.editApplicationCommunication(application.uid, [{
+function editCommunication() {
+    return airvantage.editApplicationCommunication(applicationUid, [{
         type: "MQTT",
         commIdType: "SERIAL",
         parameters: {
             password: "1234"
         }
     }]);
+}
+
+function editData() {
+    var applicationData = [];
+    applicationData.push({
+        "id": "stuff.data.temperature",
+        "label": "Temperature (°C)",
+        "description": "Temperature of the Stuff",
+        "elementType": "variable",
+        "type": "double"
+    });
+
+    applicationData.push({
+        "id": "longitude",
+        "label": "Longitude",
+        "description": "Longitude of the Stuff",
+        "elementType": "variable",
+        "type": "string"
+    });
+
+    var applicationDataDescription = [{
+        id: "stuff",
+        label: "Stuff",
+        description: "Connected stuff",
+        elementType: "node",
+        encoding: "MQTT",
+        data: applicationData
+    }];
+
+    return airvantage.editApplicationData(applicationUid, applicationDataDescription);
+}
+
+function createSystem() {
+    var serialNumber = _.uniqueId("SN");
+
+    var system = {
+        name: "System " + serialNumber,
+        state: "READY",
+        gateway: {
+            serialNumber: serialNumber,
+            labels: [label]
+        },
+        labels: [label],
+        applications: [{
+            uid: applicationUid
+        }],
+        communication: {
+            mqtt: {
+                password: "1234"
+            }
+        }
+    };
+
+    return airvantage.createSystem(system)
+        .then(function(system) {
+            console.log("Created System:", system);
+            return system;
+        });
+}
+
+function cleanResources() {
+    console.log("Clean resources");
+    return airvantage.deleteSystem({
+            labels: [label]
+        })
+        .then(function() {
+            console.log("Systems deleted");
+            return airvantage.deleteApplication({
+                labels: [label]
+            });
+        })
+        .then(function(application) {
+            console.log("Applications deleted");
+        });
 }
