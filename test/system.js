0fb8e3e765bfc58bd4913ae84a74fb9c1cf0136b
Jacques LESCOT <jlescot@sierrawireless.com>
2016-10-19 16:45:04 +0200

    :sparkles: Add a `queryAllXXX()` to all paginated APIs to return the full list of entities
    - No need to handle the recursive API calls anymore, we do that for you! :gift:
    - :warning: To be used carefully! In some cases, lots of API calls could be involved
    - Migrate to `lodash@4`

--- test/system.js
+++ test/system.js
@@ -5,21 +5,21 @@ import nock from "nock";
 import test from "ava";
 
 const airvantage = new AirVantage(config);
+
 const FAKE_UID = "fakeUid";
 
 const SYSTEM_1_UID = "3ed6cd7426604ee0bca9fe01f2521230";
 const SYSTEM_1 = {
-    "uid": SYSTEM_1_UID,
-    "name": "MySystem"
+    uid: SYSTEM_1_UID,
+    name: "MySystem"
 };
 const sampleQuerySystemsResp = {
-    "items": [SYSTEM_1, {
-        "uid": "d2cbc9d8e7b8491ea3433e6c78f984e4",
-        "name": "MyOtherSystem"
+    items: [SYSTEM_1, {
+        uid: "d2cbc9d8e7b8491ea3433e6c78f984e4",
+        name: "MyOtherSystem"
     }],
-    "count": 2,
-    "size": 2,
-    "offset": 0
+    count: 2,
+    size: 2
 };
 
 test("query systems", async t => {
@@ -38,6 +38,38 @@ test("query systems full", async t => {
 
     let systems = await airvantage.querySystems(null, true);
     t.truthy(_.isObject(systems));
+});
+
+test("query all Systems", async t => {
+    nock('https://tests.airvantage.io').get('/api/v1/systems').query(_.zipObject(["offset", "size", "access_token"], [0, 100, null])).reply(200, sampleQuerySystemsResp);
+
+    let systems = await airvantage.queryAllSystems();
+    t.truthy(_.isArray(systems));
+    t.is(_.size(systems), 2);
+});
+
+test("query all Systems with a given 'size'", async t => {
+    const firstResp = _.zipObject(["count", "size", "items"], [5, 2, _.map(["S1", "S2"], uid => _.zipObject(["uid"], [uid]))]);
+    const secondResp = _.zipObject(["count", "size", "items"], [5, 2, _.map(["S3", "S4"], uid => _.zipObject(["uid"], [uid]))]);
+    const thirdResp = _.zipObject(["count", "size", "items"], [5, 1, _.map(["S5"], uid => _.zipObject(["uid"], [uid]))]);
+
+    nock('https://tests.airvantage.io').get('/api/v1/systems').query(_.zipObject(["offset", "size", "access_token"], [0, 2, null])).reply(200, firstResp);
+    nock('https://tests.airvantage.io').get('/api/v1/systems').query(_.zipObject(["offset", "size", "access_token"], [2, 2, null])).reply(200, secondResp);
+    nock('https://tests.airvantage.io').get('/api/v1/systems').query(_.zipObject(["offset", "size", "access_token"], [4, 2, null])).reply(200, thirdResp);
+
+    let systems = await airvantage.queryAllSystems(_.zipObject(["size"], [2]));
+    t.truthy(_.isArray(systems));
+    t.is(_.size(systems), 5);
+});
+
+test("query all Systems with a given 'size' and 'offset'", async t => {
+    const lastResp = _.zipObject(["count", "size", "items"], [3, 1, _.map(["S3_Last"], uid => _.zipObject(["uid"], [uid]))]);
+
+    nock('https://tests.airvantage.io').get('/api/v1/systems').query(_.zipObject(["offset", "size", "access_token"], [2, 1, null])).reply(200, lastResp);
+
+    let systems = await airvantage.queryAllSystems(_.zipObject(["size", "offset"], [1, 2]));
+    t.truthy(_.isArray(systems));
+    t.is(_.size(systems), 1);
 });
 
 test("get systems details", async t => {
